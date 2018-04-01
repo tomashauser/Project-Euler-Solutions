e6197266a943ae0b5c457b551c6643b12a416425
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-04-01 22:22:52 +0200

    Update Problem 001.cs

--- Problem 001.cs
+++ Problem 001.cs
@@ -11,9 +11,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int Sum = 0;
-            for (int i = 1; i < 1000; i++)
+            for (int i = 1; i < 1000; i++) // Chceck first 1000 numbers.
             {
-                if (i % 3 == 0 || i % 5 == 0) Sum += i;
+                if (i % 3 == 0 || i % 5 == 0) Sum += i; // If the current numbers is divisible either by '3' or by '5' add it to 'Sum'.
             }
             Console.WriteLine(Sum);
             Console.ReadKey();
