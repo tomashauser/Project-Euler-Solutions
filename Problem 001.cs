cb9882f52a991176c6207925ad863f2b15404060
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-04-01 22:28:00 +0200

    Update Problem 001.cs

--- Problem 001.cs
+++ Problem 001.cs
@@ -1,4 +1,9 @@
-﻿using System;
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +18,7 @@ namespace ConsoleApp1
             int Sum = 0;
             for (int i = 1; i < 1000; i++) // Chceck first 1000 numbers.
             {
-                if (i % 3 == 0 || i % 5 == 0) Sum += i; // If the current numbers is divisible either by '3' or by '5' add it to 'Sum'.
+                if (i % 3 == 0 || i % 5 == 0) Sum += i; // If the current number is divisible either by '3' or '5' add it to 'Sum'.
             }
             Console.WriteLine(Sum);
             Console.ReadKey();
