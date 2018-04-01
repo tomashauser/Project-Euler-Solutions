c3120688a6ba9c0fe87daec501d19f9744beb0c2
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-04-01 20:44:49 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ namespace ConsoleApp1
 {
     class Program
     {
-        /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-        Find the sum of all the multiples of 3 or 5 below 1000.*/
         static void Main(string[] args)
         {
             int Sum = 0;
