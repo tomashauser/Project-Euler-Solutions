7f53570224c0c905d96201b8d3a65d72a0e9c922
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-04-06 17:45:17 +0200

    Update Problem 007.cs

--- Problem 007.cs
+++ Problem 007.cs
@@ -1,4 +1,9 @@
-﻿using System;
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
