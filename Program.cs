ef0ae58d3e1d2535db96d3e31920a94b5ed73d08
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-04-07 16:10:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_010
+{
+    class Program
+    {
+        static bool IsPrime(double Number)
+        {
+            for (int i = 3; i <= Math.Ceiling(Math.Sqrt(Number)); i += 2)
+            {
+                if (Number % i == 0) return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            double Sum;
+            Sum = 2; // Since 2 is the only even prime we add it to sum and operate only with odd numbers.
+            for (double i = 3; i <= 2000000; i += 2)
+            {
+                if (IsPrime(i))
+                    Sum += i;
+            }
+            Console.Write(Sum);
+            Console.ReadKey();
+            // Output: 142913828922
+        }
+    }
+}
