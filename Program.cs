5686dacd3bb11b1a012c77c08c6b3d62d887a5b2
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-04-11 17:27:11 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,23 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Numerics; // Add Numerics reference.
 
-namespace Power_digit_sum
+namespace Problem_015
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string StringNumber;
-            int Sum_Of_Digits;
-            BigInteger Number;
-            Number = BigInteger.Pow(2, 1000);
-            Sum_Of_Digits = 0;
-            StringNumber = Convert.ToString(Number);
-            for (int i = 0; i < StringNumber.Length; i++)
-            {
-                Sum_Of_Digits += Convert.ToInt32(StringNumber[i] - 48);
-            }
-            Console.WriteLine(Sum_Of_Digits);
         }
     }
 }
