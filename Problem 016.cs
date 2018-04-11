b551c8e4c095c9e1c5959865e077d5f955a26bd2
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-04-11 17:28:32 +0200

    Update and rename Program.cs to Problem 016.cs

--- Problem 016.cs
+++ Problem 016.cs
@@ -1,11 +1,16 @@
-﻿using System;
+/*
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Numerics; // Add Numerics reference.
 
-namespace Power_digit_sum
+namespace Problem_016
 {
     class Program
     {
