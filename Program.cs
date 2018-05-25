2a944d1212f8273fe3963833e56933b5c02d6b14
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-05-25 19:22:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_021 {
+    class Program {
+        static int d(int n) { // Sum of proper divisors.
+            int Sum = 1; // No need to try 1 because it is always proper divisor.
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0)
+                    Sum += i;
+            }
+            return Sum;
+        }
+        static void Main(string[] args) {
+            int Sum = 0, b;
+            for (int a = 2; a <= 10000; a++) {
+                b = d(a);
+                if (d(b) == a && a != b)
+                    Sum += a + b;
+            }
+            Console.WriteLine(Sum / 2); // Divide by 2 because program added also reversed pairs (220 & 284; 284 & 220).
+            // Output: 
+        }
+    }
+}
