371992342af6b723bce7be3732b2f86db610224d
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-05-25 19:23:55 +0200

    Update and rename Program.cs to Problem 021.cs

--- Problem 021.cs
+++ Problem 021.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +30,7 @@ namespace Problem_021 {
                     Sum += a + b;
             }
             Console.WriteLine(Sum / 2); // Divide by 2 because program added also reversed pairs (220 & 284; 284 & 220).
-            // Output: 
+            // Output: 31626
         }
     }
 }
